04416b18da2d26acf7ca393d01c44d13533bd335
Tiernan Barry <x19141840@student.ncirl.ie>
2020-02-26 16:30:01 +0000

    sentiment added to df

--- Pipeline_Stocks.py
+++ Pipeline_Stocks.py
@@ -1,8 +1,8 @@
 # Des:
 # By:
+# pip install wordcloud
 import functions_nlp as fns
 import pandas as pd
-# pip install wordcloud
 from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,35 +14,55 @@ sns.set_style('whitegrid')
 # # # # # # # # # # # # #
 
 # Get stock prices:
+# -- SPX
 spx_df = fns.alpha_v_to_df(fns.get_data_alpha_v2("SPX"))
+# -- DJIA
 djia_df = fns.alpha_v_to_df(fns.get_data_alpha_v2("DJIA"))
-combined_df = pd.DataFrame(spx_df.append(djia_df))
+# -- Combine into 1 DF
+combined_df = pd.DataFrame(spx_df.append(djia_df)) #date: yyyy-mm-dd
 
 # Get Trump Tweets:
-# Includes RTs
+# -- Includes RTs
 trump_df_all = fns.get_json_data_to_df(fns.get_trump_json_data(r"C:\Users\btier\Documents\trump_tweets.json"))
 potus_df_all = fns.get_json_data_to_df(fns.get_trump_json_data(r"C:\Users\btier\Documents\potus_tweets.json"))
-# Excludes RTs
+# -- Excludes RTs
 trump_df_nort = fns.get_json_data_to_df(fns.get_trump_json_data(r"C:\Users\btier\Documents\trump_tweets_nort.json"))
 
 # # # # # # # # # # # # #
 # Explore Data:
 # # # # # # # # # # # # #
 
-# Timeseries:
+# Trump twitter data:
+# -- Clean: Cleans trump_df_all['text'] column to 'processed text'
+trump_df_clean = fns.clean_text_words(trump_df_all)
+# ----- Ensure cleaned by checking Wordcloud:
+word_cloud = fns.get_wordcloud(trump_df_clean, r"C:\Users\btier\Documents\trump_word_cloud.png")
 
-# LDA analysis:
-# word cloud: with RT's included:
-trump_df_cln = fns.get_word_cloud(trump_df_all, r"C:\Users\btier\Documents\trump_word_cloud.png")
-# word cloud: with RT's excluded:
-trump_df_nrt_cln = fns.get_word_cloud(trump_df_nort, r"C:\Users\btier\Documents\trump_nort_wordcloud.png")
+# -- Bag of words:
+# ----- Excludes top 3 (https, tco, rt)
+top_words = fns.get_top_words(trump_df_clean)
+
+# -- Sentiment: Add sentiment to
+trump_df_clean = fns.get_sentiment_pa(trump_df_clean)
 
 
-# bag of words:
 
+
+
+
+
+
+
+
+# trump_df_clean.to_csv(r"C:\Users\btier\Documents\sentiment_trump.csv", sep=',')
+# fns.get_sentiment_nbayes(trump_df_clean)
+
+# Stock price data:
+
+
+'''
 count_vectorizer = CountVectorizer(stop_words='english')
-
-count_data = count_vectorizer.fit_transform(trump_df_nrt_cln['processed_text'])
+count_data = count_vectorizer.fit_transform(trump_df_clean['processed_text'])
 
 def plt_top_20_words(count, count_vect):
     single_words = count_vect.get_feature_names()
@@ -56,7 +76,7 @@ def plt_top_20_words(count, count_vect):
     counts = [i[1] for i in count_dict]
     return [single_words, counts]
 
-    '''
+
     x_pos = np.arange(len(words))
 
     plt.figure(2, figsize=(15, 15 / 1.6180))
@@ -66,7 +86,7 @@ def plt_top_20_words(count, count_vect):
     plt.xticks(x_pos, words, rotation=90)
     plt.xlabel('words')
     plt.ylabel('counts')
-    return plt.show()'''
+    return plt.show()
 
 plt_top_20_words(count_data,count_vectorizer)[0]
 
@@ -114,3 +134,4 @@ plt.
 
 
 
+'''
